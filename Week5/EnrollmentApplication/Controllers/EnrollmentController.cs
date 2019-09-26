e690a7db305970fc20e48a1f9343f74fe368fc82
Todd Liddy <Toddj.liddy@gmail.com>
2019-09-26 18:54:51 -0400

    Friday PM commit

--- Week5/EnrollmentApplication/Controllers/EnrollmentController.cs
+++ Week5/EnrollmentApplication/Controllers/EnrollmentController.cs
@@ -46,7 +46,7 @@ namespace EnrollmentApplication.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "EnrollmentID,StudentID,CourseID")] Enrollment enrollment)
+        public ActionResult Create([Bind(Include = "EnrollmentID,StudentID,CourseID,IsActive,AssignedCampus,EnrollmentSemester,EnrollmentYear")] Enrollment enrollment)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace EnrollmentApplication.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "EnrollmentID,StudentID,CourseID")] Enrollment enrollment)
+        public ActionResult Edit([Bind(Include = "EnrollmentID,StudentID,CourseID,IsActive,AssignedCampus,EnrollmentSemester,EnrollmentYear")] Enrollment enrollment)
         {
             if (ModelState.IsValid)
             {
--- Week5/EnrollmentApplication/Models/Enrollment.cs
+++ Week5/EnrollmentApplication/Models/Enrollment.cs
@@ -13,5 +13,10 @@ namespace EnrollmentApplication.Models
         public virtual char Grade { get; set; }
         public virtual object Student { get; set; }
         public virtual object Course { get; set; }
+        public virtual Boolean IsActive { get; set; }
+        public virtual string AssignedCampus { get; set; }
+        public virtual string EnrollmentSemester { get; set; }
+        public virtual int EnrollmentYear { get; set; }
+
     }
 }
