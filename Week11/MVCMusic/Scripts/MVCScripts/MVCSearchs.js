e559daa2930f1dc109a7ddf90bc3074ca6054b7f
Todd Liddy <Toddj.liddy@gmail.com>
2019-11-10 22:01:57 -0500

    111019 pm

--- Week11/MVCMusic/Scripts/MVCScripts/MVCSearchs.js
+++ Week11/MVCMusic/Scripts/MVCScripts/MVCSearchs.js
@@ -1,3 +1,17 @@
 ﻿function ArtistFailed() {
     $("#searchresults").html("Unable to locate Artist");
 }
+
+$(function () {
+    $(".RemoveLInk").click(function () {
+        var id = $(this).attr("data-id")
+        $.post("/ShoppingCart/RemoveFromCart", { "id": id }, function (data) {
+            $("#update-message").text(data.Message);
+            $("#cart-total").text(data.CartTotal);
+            $("#item-count-" + data.DeleteID).text(data.ItemCount);
+            if (data.ItemCount < 1) {
+                $("record-" + data.DeleteID).fadeOut();
+            }
+        });
+    })
+});
