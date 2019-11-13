cd064c22f1f9bf1f4a5a5c04be2853c15792985f
Todd Liddy <Toddj.liddy@gmail.com>
2019-11-13 13:31:55 -0500

    111319 initial

---
+++ Week12/MVCMusic/Migrations/201911122047101_initial.cs
@@ -0,0 +1,78 @@
+namespace MVCMusicStoreApplication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Albums",
+                c => new
+                    {
+                        AlbumId = c.Int(nullable: false, identity: true),
+                        GenreId = c.Int(nullable: false),
+                        ArtistId = c.Int(nullable: false),
+                        Title = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        AlbumArtUrl = c.String(),
+                        CountryOfOrigin = c.String(),
+                        InStock = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.AlbumId)
+                .ForeignKey("dbo.Artists", t => t.ArtistId, cascadeDelete: true)
+                .ForeignKey("dbo.Genres", t => t.GenreId, cascadeDelete: true)
+                .Index(t => t.GenreId)
+                .Index(t => t.ArtistId);
+            
+            CreateTable(
+                "dbo.Artists",
+                c => new
+                    {
+                        ArtistId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.ArtistId);
+            
+            CreateTable(
+                "dbo.Genres",
+                c => new
+                    {
+                        GenreId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.GenreId);
+            
+            CreateTable(
+                "dbo.Carts",
+                c => new
+                    {
+                        RecordID = c.Int(nullable: false, identity: true),
+                        CartID = c.String(),
+                        AlbumID = c.Int(nullable: false),
+                        Count = c.Int(nullable: false),
+                        DateCreated = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.RecordID)
+                .ForeignKey("dbo.Albums", t => t.AlbumID, cascadeDelete: true)
+                .Index(t => t.AlbumID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Carts", "AlbumID", "dbo.Albums");
+            DropForeignKey("dbo.Albums", "GenreId", "dbo.Genres");
+            DropForeignKey("dbo.Albums", "ArtistId", "dbo.Artists");
+            DropIndex("dbo.Carts", new[] { "AlbumID" });
+            DropIndex("dbo.Albums", new[] { "ArtistId" });
+            DropIndex("dbo.Albums", new[] { "GenreId" });
+            DropTable("dbo.Carts");
+            DropTable("dbo.Genres");
+            DropTable("dbo.Artists");
+            DropTable("dbo.Albums");
+        }
+    }
+}
