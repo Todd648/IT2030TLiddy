c53c836fd72743a50dc59ba3b826c00cb1148b95
Todd Liddy <Toddj.liddy@gmail.com>
2019-11-18 16:47:23 -0500

    111819

---
+++ Week12/MovieStore.Tests/Controllers/MovieStoreControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieStore.Controllers;
+using MovieStore.Models;
+
+namespace MovieStore.Tests.Controllers
+{
+    [TestClass]
+    public class MovieStoreControllerTest
+    {
+        [TestMethod]
+        public void MovieStore_Index_TestView()
+        {
+            //Arrange
+            MoviesController controller = new MoviesController();
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void MovieStore_ListOfMovies()
+        {
+            //Arrange
+            MoviesController controller = new MoviesController();
+            //Act
+            List<Movie> result = controller.ListOfMovies();
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Iron Man 1", result[0].Title);
+            Assert.AreEqual("Iron Man 2", result[1].Title);
+            Assert.AreEqual("Iron Man 3", result[2].Title);
+        }
+        [TestMethod]
+        public void MovieStore_Index_Redirect()
+        {
+            //Arrange
+            MoviesController controller = new MoviesController();
+            //Act
+            RedirectToRouteResult result = controller.IndexRedirect() as RedirectToRouteResult;
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Create", result.RouteValues["action"]);
+        }
+
+    }
+}
---
+++ Week12/MovieStore/Controllers/MoviesController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MovieStore.Models;
+using MovieStore.Controllers;
+using System.Collections.Generic;
+
+namespace MovieStore.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private MovieStoreDBContext db = new MovieStoreDBContext();
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListFromDB()
+        {
+            List<Movie> movies = this.db.Movies.ToList();
+            return this.View(movies);
+        }
+
+        public List<Movie> ListOfMovies()
+        {
+            List<Movie> movies = new List<Movie>
+            {
+                new Movie {MovieID = 1, Title = "Iron Man 1"},
+                new Movie {MovieID = 2, Title = "Iron Man 2"},
+                new Movie {MovieID = 3, Title = "Iron Man 3"}
+            };
+            return movies;
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+        public ActionResult IndexRedirect()
+        {
+            return RedirectToAction("Create");
+        }
+        // GET: Movies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MovieID,Title,ReleaseYear")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movie);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MovieID,Title,ReleaseYear")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Movie movie = db.Movies.Find(id);
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
