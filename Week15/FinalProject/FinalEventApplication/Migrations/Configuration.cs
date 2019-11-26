dd2eb0de6ddf17a34e5b3b488f401706889c2ed9
Todd Liddy <Toddj.liddy@gmail.com>
2019-11-26 14:57:10 -0500

    112619 pm 2

--- Week15/FinalProject/FinalEventApplication/Migrations/Configuration.cs
+++ Week15/FinalProject/FinalEventApplication/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace FinalEventApplication.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<FinalEventApplication.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<FinalEventApplication.Models.FinalEventApplicationDB>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(FinalEventApplication.Models.ApplicationDbContext context)
+        protected override void Seed(FinalEventApplication.Models.FinalEventApplicationDB context)
         {
             //  This method will be called after migrating to the latest version.
 
